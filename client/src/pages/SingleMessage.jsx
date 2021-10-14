b1c50b015d2b068332314de2398276e648abe53c
Sally Alvarenga <sallyalvarenga35@gmail.com>
2021-10-14 08:52:44 -0500

    modifiied the models and seeds by adding the messagePost. about to finalize typeDef and Resolvers

---
+++ client/src/pages/SingleMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Import the `useParams()` hook
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+
+import CommentList from "../components/CommentList";
+import CommentForm from "../components/CommentForm";
+
+import { QUERY_SINGLE_THOUGHT } from "../utils/queries";
+
+export default SingleMessage;
