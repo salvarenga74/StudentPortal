424319bcceb9be7d679214c56caaa28ac8cc97e9
Eric Duwe <ericduwe@gmail.com>
2021-10-18 14:13:27 -0500

    fixed CSS for readability and added README

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -8,12 +8,12 @@ const CommentList = ({ comments = [] }) => {
   return (
     <>
       <h3
-        className="p-5 display-inline-block"
-        style={{ borderBottom: '1px dotted #1a1a1a' }}
+        className="box"
+        
       >
         Comments
       </h3>
-      <div className="flex-row my-4">
+      <div className="row">
         {comments &&
           comments.map((comment) => (
             <div key={comment._id} className="col-12 mb-3 pb-3">
